e59e8e7548e3b488e1d0efac147c954d8da03493
Walter Vinicius <walter.cardoso@socialminer.com>
2020-10-30 09:19:28 -0300

    mock call to stur

---
+++ Poc.Puc.SGM/src/Services/Poc.Puc.SGM.SupportCitizens/Controllers/CitizenSupportController.cs
@@ -0,0 +1,18 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using Poc.Puc.SGM.SupportCitizens.Services;
+
+namespace Poc.Puc.SGM.SupportCitizens.Controllers
+{
+    [Route("citizen/support")]
+    [ApiController]
+    public class CitizenSupportController : ControllerBase
+    {   
+        [Route("{identifier}")]
+        public IActionResult GetByIdentifier([FromRoute] string identifier)
+        {
+            var service = new STURService();
+
+            return Ok(service.GetByIdentifier(identifier));
+        }
+    }
+}
---
+++ Poc.Puc.SGM/src/Services/Poc.Puc.SGM.SupportCitizens/Services/STURService.cs
@@ -0,0 +1,29 @@
+﻿using Poc.Puc.SGM.SupportCitizens.Domain;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace Poc.Puc.SGM.SupportCitizens.Services
+{
+    public class STURService
+    {
+        private IEnumerable<Citizen> Citizens;
+
+        public STURService()
+        {
+            Citizens = new List<Citizen>()
+            {
+                new Citizen() { Identifier = "44166763930", FirstName = "João", LastName = "Silva", Fee = 100, Tax = 10, Zone = "Rural" },
+                new Citizen() { Identifier = "44166763933", FirstName = "Leonardo", LastName = "Silva", Fee = 100, Tax = 10, Zone = "Cidade" },
+                new Citizen() { Identifier = "44166763932", FirstName = "Maria", LastName = "Silva", Fee = 100, Tax = 10, Zone = "Desconhecido" },
+                new Citizen() { Identifier = "44166763931", FirstName = "´José", LastName = "Silva", Fee = 100, Tax = 10, Zone = "Cidade" },
+            };
+        }
+
+        public Citizen GetByIdentifier(string identifier)
+        {
+            return Citizens.FirstOrDefault(x => x.Identifier == identifier);
+        }
+    }
+}
