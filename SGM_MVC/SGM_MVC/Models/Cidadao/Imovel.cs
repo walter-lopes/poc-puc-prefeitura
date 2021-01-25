8cae482ac10fe2b6a7ed3f004d298c8f316a3362
Kleber Santos <sdw256@gmail.com>
2021-01-25 18:09:20 -0300

    Adequação de classes

--- SGM_MVC/SGM_MVC/Models/Cidadao/Imovel.cs
+++
@@ -1,9 +0,0 @@
-﻿namespace SGM_MVC.Models.Cidadao
-{
-    public class Imovel
-    {
-        public string TipoImovel { get; set; }
-        public string TipoImposto { get; set; }
-        public Endereco Endereco { get; set; }
-    }
-}
